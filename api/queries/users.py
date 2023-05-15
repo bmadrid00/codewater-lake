5081f3be613070d52df543b9f7ee818119296a94
bileE <eric.biles@proton.me>
2023-05-15 13:53:18 -0700

    added user routes

--- api/queries/users.py
+++ api/queries/users.py
@@ -15,57 +15,56 @@ class UserIn(BaseModel):
 class UserOut(UserIn):
     id: int
 
-
-def get_user(self, user_id: int) -> UserOut:
-    with pool.connection() as conn:
-        with conn.cursor() as db:
-            db.execute(
-                """
-                SELECT id, first_name, last_name, email
-                FROM users
-                WHERE id=%s
-                """,
-                [user_id]
-                )
-            result = db.fetchone()
-            user = {}
-            for i, col in enumerate(db.description):
-                user[col.name] = result[i]
-            return user
-
-
-def delete_user(self, user_id: int) -> bool:
-    try:
+class UserQueries():
+    def get_user(self, user_id: int) -> UserOut:
         with pool.connection() as conn:
             with conn.cursor() as db:
                 db.execute(
                     """
-                    DELETE FROM users
+                    SELECT id, first_name, last_name, email
+                    FROM users
                     WHERE id=%s
                     """,
                     [user_id]
+                    )
+                result = db.fetchone()
+                user = {}
+                for i, col in enumerate(db.description):
+                    user[col.name] = result[i]
+                return user
+
+
+    def delete_user(self, user_id: int) -> bool:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        DELETE FROM users
+                        WHERE id=%s
+                        """,
+                        [user_id]
+                    )
+            return True
+        except Exception:
+            return {"message": "Invalid user id"}
+
+
+    def update_user(self, user_id: int, user: UserIn) -> UserOut
+        with pool.connection() as conn:
+            with conn.cursor() as db:
+                db.execute(
+                    """
+                    UPDATE users
+                    SET user first_name=%s
+                        , last_name=%s
+                        , email=%s
+                    WHERE id=%s
+                    """,
+                    [user.first_name,
+                    user.last_name,
+                    user.email,
+                    user_id]
                 )
-        return True
-    except Exception:
-        return {"message": "Invalid user id"}
-
-
-def update_user(self, user_id: int, user: UserIn) -> UserOut
-    with pool.connection() as conn:
-        with conn.cursor() as db:
-            db.execute(
-                """
-                UPDATE users
-                SET user first_name=%s
-                    , last_name=%s
-                    , email=%s
-                WHERE id=%s
-                """,
-                [user.first_name,
-                 user.last_name,
-                 user.email,
-                 user_id]
-            )
-            old_data = user.dict()
-            return UserOut(id=user_id, **old_data)
-
+                old_data = user.dict()
+                return UserOut(id=user_id, **old_data)
