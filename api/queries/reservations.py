19ab565bee6fc4ebdf04f5d5755f4a8f22f5325f
Jobanpreet Singh <leonj7153@gmail.com>
2023-05-15 16:27:39 +0000

    Complete migrations; start queries and routers

---
+++ api/queries/reservations.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from datetime import date
+
+
+class ReservationIn(BaseModel):
+    cabin_id: int
+    start_date: date
+    end_date: date
+    user_id: int
+    number_of_people: int
+
+
+class ReservationOut(ReservationIn):
+    id: int
