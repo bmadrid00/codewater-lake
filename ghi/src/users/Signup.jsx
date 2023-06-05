f2a8d995d488b74027aa8b10fc49b1af22f5a3ee
Paul Kingston <paul.jesse.is@gmail.com>
2023-06-05 19:43:29 +0000

    Login styling

--- ghi/src/users/Signup.jsx
+++ ghi/src/users/Signup.jsx
@@ -1,96 +1,112 @@
 import React, { useState } from "react";
-import { MDBBtn } from "mdb-react-ui-kit";
-import { useSignupMutation } from "../redux/apiSlice";
+import { useLoginMutation, useSignupMutation } from "../redux/apiSlice";
 import { useNavigate } from "react-router-dom";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn } from "mdb-react-ui-kit";
 
 function Signup() {
   const [register] = useSignupMutation();
   const navigate = useNavigate();
-  const [first_name, setFirstname] = useState("");
-  const [last_name, setLastname] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmpassword, setConfirmpassword] = useState("");
+  const [first_name, setFirstname] = useState(null);
+  const [last_name, setLastname] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [confirmpassword, setConfirmpassword] = useState(null);
   const [errorMessage, setErrorMessage] = useState("");
+  const [ login ] = useLoginMutation()
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (password !== confirmpassword) {
       setErrorMessage("Passwords entered do not match.");
     } else {
-      register({ first_name, last_name, email, password });
-      navigate("/");
-      console.log("Form submitted:", {
-        first_name,
-        last_name,
-        email,
-        password,
-      });
+      const response = await register({ first_name, last_name, email, password });
+      console.log(response)
+      if (response.error) {
+        alert("Incomplete form!", response.error.status);
+      } else {
+        login({ email, password });
+        navigate("/");
+        alert("Account created!");
+        console.log("Form submitted:", {
+          first_name,
+          last_name,
+          email,
+          password,
+        });
+      }
+
     }
   };
 
   return (
     <>
-      <div className="card text-bg-light mb-3">
-        <h5 className="card-header">Signup</h5>
-        <div className="card-body">
-          {errorMessage && (
-            <div className="alert alert-danger" role="alert">
-              {errorMessage}
+      <MDBContainer className="signup">
+        <MDBRow>
+          <MDBCol>
+            <div className="card text-bg-light mb-3">
+              <h5 className="card-header">Signup</h5>
+              <div className="card-body">
+                {errorMessage && (
+                  <div className="alert alert-danger" role="alert">
+                    {errorMessage}
+                  </div>
+                )}
+                <form onSubmit={(e) => handleSubmit(e)}>
+                  <div className="mb-3">
+                    <label className="form-label">Enter Your First Name</label>
+                    <input
+                      name="first name"
+                      type="text"
+                      className="form-control"
+                      onChange={(e) => setFirstname(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Enter Your Last Name</label>
+                    <input
+                      name="last name"
+                      type="text"
+                      className="form-control"
+                      onChange={(e) => setLastname(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Enter Email</label>
+                    <input
+                      name="email"
+                      type="text"
+                      className="form-control"
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Enter Password</label>
+                    <input
+                      name="password"
+                      type="password"
+                      className="form-control"
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Confirm Password</label>
+                    <input
+                      name="confirmed password"
+                      type="password"
+                      className="form-control"
+                      onChange={(e) => setConfirmpassword(e.target.value)}
+                    />
+                  </div>
+                  <MDBBtn outline rounded type="submit" color="info" block>
+                    Sign up
+                  </MDBBtn>
+                </form>
+              </div>
             </div>
-          )}
-          <form onSubmit={(e) => handleSubmit(e)}>
-            <div className="mb-3">
-              <label className="form-label">Enter Your First Name</label>
-              <input
-                name="first name"
-                type="text"
-                className="form-control"
-                onChange={(e) => setFirstname(e.target.value)}
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Enter Your Last Name</label>
-              <input
-                name="last name"
-                type="text"
-                className="form-control"
-                onChange={(e) => setLastname(e.target.value)}
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Enter Email</label>
-              <input
-                name="email"
-                type="text"
-                className="form-control"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Enter Password</label>
-              <input
-                name="password"
-                type="password"
-                className="form-control"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Confirm Password</label>
-              <input
-                name="confirmed password"
-                type="password"
-                className="form-control"
-                onChange={(e) => setConfirmpassword(e.target.value)}
-              />
-            </div>
-            <div>
-              <MDBBtn className="btn btn-primary" type="submit" >Sign Up</MDBBtn>
-            </div>
-          </form>
-        </div>
-      </div>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
     </>
   );
 }
