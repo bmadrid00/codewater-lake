a0f62e788bd8efe00204b270ded645ada70b2346
paul.jesse.is <paul.jesse.is@gmail.com>
2023-06-07 11:28:37 -0600

    some code formatting and cleanup

--- ghi/src/reservations/ReservationForm.jsx
+++ ghi/src/reservations/ReservationForm.jsx
@@ -64,7 +64,6 @@ function ReservationForm(props) {
         start_date: form.start_date.value,
         end_date: form.end_date.value,
         };
-        console.log(reservation);
         await bookReservation(reservation);
     }
 
@@ -112,74 +111,74 @@ function ReservationForm(props) {
             </MDBModalDialog>
         </MDBModal>
         <div className="card text-bg-light text-center mb-3">
-        <h5 className="card-header mb-1">Book a Reservation</h5>
-        <div className="card-body">
-            <Form onSubmit={handleSubmit}>
-                <MDBContainer>
-                    <MDBRow start>
-                        <MDBCol size='md'>
-                            <div>
-                                <label htmlFor="cabin">Cabin Selection:</label>
-                                <select className="form-input"
-                                    key="cabin"
-                                    value={cabinID}
-                                    onChange={(e) => {
-                                    dispatch(assignCabin(e.target.value));
-                                    }}
-                                    required
-                                    name="cabin"
-                                    id="cabin">
-                                    <option key={0} value="">Choose a cabin</option>
-                                    {usefulCabinsData.map((cabin) => (
-                                        <option key={cabin.id} value={cabin.id}>
-                                            {cabin.cabin_name}
-                                        </option>
-                                    ))}
-                                </select>
-                            </div>
-                        </MDBCol>
-                        <MDBCol size='md'>
-                        <label htmlFor="occupancy">Number of People</label>
-                        <input className="form-input"
-                            key="occupancy"
-                            value={occupancy}
-                            id="occupancy"
-                            name="occupancy"
-                            type="number"
-                            required
-                            onChange={(e) => setOccupancy(e.target.value)}
-                        />
-                        </MDBCol>
-                        <MDBCol size='md'>
-                        <label htmlFor="start_date">Start Date:</label>
-                        <input className="form-input"
-                            key="start_date"
-                            value={start_date}
-                            id="start_date"
-                            name="start_date"
-                            type="date"
-                            required
-                            onChange={(e) => setStartDate(e.target.value)}
-                        />
-                        </MDBCol>
-                        <MDBCol size='md'>
-                        <label htmlFor="end_date">End Date:</label>
-                        <input className="form-input"
-                            key="end_date"
-                            value={end_date}
-                            id="end_date"
-                            name="end_date"
-                            type="date"
-                            required
-                            onChange={(e) => setEndDate(e.target.value)}
-                        />
-                        </MDBCol>
-                        <MDBCol size='md'><button className="btn btn-primary">Book It!</button></MDBCol>
-                    </MDBRow>
-                </MDBContainer>
-            </Form>
+            <h5 className="card-header mb-1">Book a Reservation</h5>
+            <div className="card-body">
+                <Form onSubmit={handleSubmit}>
+                    <MDBContainer>
+                        <MDBRow start>
+                            <MDBCol size='md'>
+                                <div>
+                                    <label htmlFor="cabin">Cabin Selection:</label>
+                                    <select className="form-input"
+                                        key="cabin"
+                                        value={cabinID}
+                                        onChange={(e) => {
+                                        dispatch(assignCabin(e.target.value));
+                                        }}
+                                        required
+                                        name="cabin"
+                                        id="cabin">
+                                        <option key={0} value="">Choose a cabin</option>
+                                        {usefulCabinsData.map((cabin) => (
+                                            <option key={cabin.id} value={cabin.id}>
+                                                {cabin.cabin_name}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                            </MDBCol>
+                            <MDBCol size='md'>
+                            <label htmlFor="occupancy">Number of People</label>
+                            <input className="form-input"
+                                key="occupancy"
+                                value={occupancy}
+                                id="occupancy"
+                                name="occupancy"
+                                type="number"
+                                required
+                                onChange={(e) => setOccupancy(e.target.value)}
+                            />
+                            </MDBCol>
+                            <MDBCol size='md'>
+                            <label htmlFor="start_date">Start Date:</label>
+                            <input className="form-input"
+                                key="start_date"
+                                value={start_date}
+                                id="start_date"
+                                name="start_date"
+                                type="date"
+                                required
+                                onChange={(e) => setStartDate(e.target.value)}
+                            />
+                            </MDBCol>
+                            <MDBCol size='md'>
+                            <label htmlFor="end_date">End Date:</label>
+                            <input className="form-input"
+                                key="end_date"
+                                value={end_date}
+                                id="end_date"
+                                name="end_date"
+                                type="date"
+                                required
+                                onChange={(e) => setEndDate(e.target.value)}
+                            />
+                            </MDBCol>
+                            <MDBCol size='md'><button className="btn btn-primary">Book It!</button></MDBCol>
+                        </MDBRow>
+                    </MDBContainer>
+                </Form>
+            </div>
         </div>
-      </div>
     </div>
     );
 }
