f98eb4e403850513dd5d3b2f37ab3447bd6388f9
luis Vela <velaluis60@gmail.com>
2023-06-09 19:05:46 +0000

    Merge weather to main

--- api/tests/test_list_cabins.py
+++ api/tests/test_list_cabins.py
@@ -12,30 +12,7 @@ class FakeCabinQueries():
         return {'cabins': []}
 
     def get_cabin(self, cabin_id: int):
-        return {
-            "id": 1,
-            "cabin_name": "Maple",
-            "max_occupants": 4,
-            "description": """Perched on the edge of a serene lake, this small
-            moder lakeside house offers a harmonious blend of contemporary design
-            and natural beauty. With expansive windows framing breathtaking views,
-            the sleek exterior seamlessly integrates wood, stone, and glass.
-            Inside, an open-concept living space with minimalist aesthetics and
-            abundant natural light awaits. Two cozy bedrooms with picturesque
-            vistas provide a tranquil escape. Step outside to a deck extending
-            towards the water, where relaxation and connection with nature await.
-            This idyllic retreat offers a peaceful lakeside lifestyle in a compact,
-            modern setting.""",
-            "on_lake": True,
-            "rating": 4.8,
-            "day_rate": 35000,
-            "cabin_images": [
-                "https://dl.dropboxusercontent.com/s/4zhx7opb9lr8jbg/exterior-with-lake.webp",
-                "https://dl.dropboxusercontent.com/s/ub7pgw8b83bc5jw/living-room.webp",
-                "https://dl.dropboxusercontent.com/s/yvo4vq512amwn84/bedroom.webp",
-                "https://dl.dropboxusercontent.com/s/1ri9wsvzxwyxinc/bathroom.webp"
-            ]
-        }
+        pass
 
     def create_cabin(self, cabin: CabinIn):
         pass
@@ -60,39 +37,3 @@ def test_list_cabins():
     assert data == {'cabins': []}
 
     app.dependency_overrides = {}
-
-
-def test_get_cabin():
-    # Arrange
-    app.dependency_overrides[CabinQueries] = FakeCabinQueries
-
-    # Act
-    res = client.get('/api/cabins/1')
-    data = res.json()
-
-    # Assert
-    assert res.status_code == 200
-    assert data == {
-            "id": 1,
-            "cabin_name": "Maple",
-            "max_occupants": 4,
-            "description": """Perched on the edge of a serene lake, this small
-            moder lakeside house offers a harmonious blend of contemporary design
-            and natural beauty. With expansive windows framing breathtaking views,
-            the sleek exterior seamlessly integrates wood, stone, and glass.
-            Inside, an open-concept living space with minimalist aesthetics and
-            abundant natural light awaits. Two cozy bedrooms with picturesque
-            vistas provide a tranquil escape. Step outside to a deck extending
-            towards the water, where relaxation and connection with nature await.
-            This idyllic retreat offers a peaceful lakeside lifestyle in a compact,
-            modern setting.""",
-            "on_lake": True,
-            "rating": 4.8,
-            "day_rate": 35000,
-            "cabin_images": [
-                "https://dl.dropboxusercontent.com/s/4zhx7opb9lr8jbg/exterior-with-lake.webp",
-                "https://dl.dropboxusercontent.com/s/ub7pgw8b83bc5jw/living-room.webp",
-                "https://dl.dropboxusercontent.com/s/yvo4vq512amwn84/bedroom.webp",
-                "https://dl.dropboxusercontent.com/s/1ri9wsvzxwyxinc/bathroom.webp"
-            ]
-        }
---
+++ ghi/src/users/weather.jsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+  MDBCardTitle,
+} from "mdb-react-ui-kit";
+
+const Weather = () => {
+  const [apiData, setApiData] = useState();
+  const API_KEY = "7a1c3fca54d378c6d010e3a3632b7f43";
+  const API_ENDPOINT = `https://api.openweathermap.org/data/2.5/forecast`;
+  const DEFAULT_CITY = "Kalispell";
+  const weatherIconUrl = "http://openweathermap.org/img/wn/";
+
+const weatherByDay = () => {
+  if (!apiData) {
+    return [];
+  }
+  const dailyData = {};
+  for (const forecast of apiData.list) {
+    const date = forecast.dt_txt.split(" ")[0];
+    if (!dailyData[date]) {
+      dailyData[date] = forecast;
+    }
+  }
+  const dailyForecasts = Object.values(dailyData);
+  return dailyForecasts.slice(0, 5);
+};
+
+const renderForecast = () => {
+  const dailyForecast = weatherByDay();
+  return dailyForecast.map((forecast, index) => {
+  const { icon } = forecast.weather[0];
+  const iconUrl = `${weatherIconUrl}${icon}@2x.png`;
+  const tempInFahrenheit = Math.round(
+    ((forecast.main.temp - 273.15) * 9) / 5 + 32
+  );
+      return (
+        <div key={index}>
+          <div>
+            {Math.round(tempInFahrenheit)}°F
+          </div>
+          <img
+            src={iconUrl}
+            alt="weather icon"
+            style={{ width: "4em", height: "4em" }}
+            />
+          <div>
+              {new Date(forecast.dt_txt).toLocaleDateString("en-US", {
+                weekday: "short",
+              })}
+          </div>
+        </div>
+      );
+    });
+  };
+
+useEffect(() => {
+  const fetchData = async () => {
+    const response = await fetch(
+      `${API_ENDPOINT}?q=${DEFAULT_CITY}&appid=${API_KEY}`
+    );
+    const data = await response.json();
+    if (response.ok) {
+      setApiData(data);
+    } 
+  };
+
+  fetchData();
+}, [API_KEY, API_ENDPOINT]);
+
+  return (
+    <>
+      <MDBContainer>
+        <MDBRow
+          className="justify-content-center align-items-center h-100">
+          <MDBCol md="8" lg="10" xl="">
+            <MDBCard className="mb-2" style={{ borderRadius: "25px" }}>
+              <MDBCardBody className="p-3">
+                <MDBCardTitle className="text-center"> 5 DAY FORECAST</MDBCardTitle>
+                <div className="d-flex justify-content-around text-center">
+                  {renderForecast()}
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </>
+  );
+};
+
+export default Weather;
